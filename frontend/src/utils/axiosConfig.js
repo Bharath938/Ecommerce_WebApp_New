cfb0f7a86978cff172742ac09f0572e41245475a
Bharath <148990264+Bharath938@users.noreply.github.com>
2026-05-18 12:50:20 +0530

    Update axios base URL to use environment variable

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ecommerce-webapp-new-1.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use(
